d3de4056cacf149c3a33be9cd41eecc1aad559b3
Narendra Kumar <Narendra.k@antino.io>
2023-01-09 12:27:25 +0530

    project model

---
+++ models/Project.js
@@ -0,0 +1,16 @@
+let mongoose = require('mongoose')
+
+const { Schema } = mongoose
+
+const ProjectSchema = new Schema({
+    name: {
+        type: mongoose.Schema.Types.String,
+        required: true
+    },
+    description: {
+        type: mongoose.Schema.Types.String
+    },
+    status: {
+        type: mongoose.Schema.Types.String,
+    }
+})                                         
